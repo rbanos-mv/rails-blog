1ce0be022c021dc2e598ef2030fdec23282c4d39
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-10 08:11:39 -0500

    Add Like mode class

---
+++ app/models/like.rb
@@ -0,0 +1,14 @@
+# Like class
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+  # adding "counter_cache: true" to the previous line automatically
+  # increments the counter, and it is not necessary the update_likes_count method
+  after_save :update_likes_count
+
+  private
+
+  def update_likes_count
+    post.increment!(:likes_count)
+  end
+end
