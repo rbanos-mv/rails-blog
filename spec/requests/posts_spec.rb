e38e6ea9eeb40f7a15741348646c305183296912
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-18 05:09:20 -0500

    Replace constants with path helpers in tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe 'The Blog App', type: :request do
   describe 'correctly handles the route posts#index' do
     it '( recognizes )' do
-      assert_recognizes({ controller: 'posts', action: 'index', user_id: '1' }, '/users/1/posts')
+      assert_recognizes({ controller: 'posts', action: 'index', user_id: '1' }, user_posts_path(1))
     end
 
     it '( generates )' do
-      assert_generates '/users/1/posts', { controller: 'posts', action: 'index', user_id: '1' }
+      assert_generates user_posts_path(1), { controller: 'posts', action: 'index', user_id: '1' }
     end
   end
 
   describe 'correctly handles the route posts#show' do
     it '( recognizes )' do
-      assert_recognizes({ controller: 'posts', action: 'show', user_id: '1', id: '1' }, '/users/1/posts/1')
+      assert_recognizes({ controller: 'posts', action: 'show', user_id: '1', id: '1' }, user_post_path(1, 1))
     end
 
     it '( generates )' do
-      assert_generates '/users/1/posts/1', { controller: 'posts', action: 'show', user_id: '1', id: '1' }
+      assert_generates user_post_path(1, 1), { controller: 'posts', action: 'show', user_id: '1', id: '1' }
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe 'The Blog App', type: :request do
   describe 'correctly handles the route users#index' do
     it '( recognizes )' do
-      assert_recognizes({ controller: 'users', action: 'index' }, '/users')
+      assert_recognizes({ controller: 'users', action: 'index' }, users_path)
     end
 
     it '( generates )' do
-      assert_generates '/users', { controller: 'users', action: 'index' }
+      assert_generates root_path, { controller: 'users', action: 'index' }
     end
   end
 
   describe 'correctly handles the route users#show' do
     it '( recognizes )' do
-      assert_recognizes({ controller: 'users', action: 'show', id: '1' }, '/users/1')
+      assert_recognizes({ controller: 'users', action: 'show', id: '1' }, user_path(1))
     end
 
     it '( generates )' do
-      assert_generates '/users/1', { controller: 'users', action: 'show', id: '1' }
+      assert_generates user_path(1), { controller: 'users', action: 'show', id: '1' }
     end
   end
 end
