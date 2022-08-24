a93ea53f7335fa49ed0924e43b03cf07f92070ec
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-24 06:22:54 -0500

    Refactor to add default photo when null

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def author_photo
+    photo.blank? ? 'user.png' : photo
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
