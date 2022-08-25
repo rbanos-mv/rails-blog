131879459025f884f5ea16c904c3d269de58893d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-25 14:48:51 -0500

    Refactor to allow deletion

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,19 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
   # adding "counter_cache: true" to the previous line automatically
   # increments the counter, and no longer is necessary the update_comments_count method
-  after_save :update_comments_count
+  after_save :increment_comments_count
+  after_destroy :decrement_comments_count
 
   private
 
-  def update_comments_count
+  def increment_comments_count
     post.increment!(:comments_count)
+  end
+
+  def decrement_comments_count
+    post.decrement!(:comments_count)
   end
 end
